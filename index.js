8cbf0aa429265f02d345e5f46c46885a6819c957
AChar1f <acharif41@gmail.com>
2024-07-31 14:28:49 +0200

    answer comment inserted

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ fs.writeFile('data.txt', 'Welcome to Node.js!', function (err){
     console.log('Saved')
 })
 
-fs.writeFile('data.txt', ' If you reading this the previous content was replaced.', function (err) {
+fs.writeFile('data.txt', ' If you reading this content, it has replaced the previous content.', function (err) {
     if (err) throw err;
     console.log('Replaced!')
 })
-fs.appendFile('data.txt', 'The content you are reading was appended to the document.', function (err){
+//  The Second Content has overwritten the previous content.
+
+fs.appendFile('data.txt', ' The content you are reading was appended to the document.', function (err){
     if (err) throw err;
     console.log('Updated!')
 })
-
-//  Content
